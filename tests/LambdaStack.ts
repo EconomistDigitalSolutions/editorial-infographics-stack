57b9b3b406b2518d6aa6a8ef9be015a1377d2e1f
Liam Bird <liambird@economist.com>
2023-06-05 15:55:28 +0100

    Fixed tests

--- tests/LambdaStack.ts
+++ tests/LambdaStack.ts
@@ -1,6 +1,5 @@
 import { Template, Match } from 'aws-cdk-lib/assertions';
-import { Duration, Stack } from 'aws-cdk-lib';
-import { Runtime } from 'aws-cdk-lib/aws-lambda';
+import { Stack } from 'aws-cdk-lib';
 
 import LambdaStack from '../lib/stacks/Lambda';
 
@@ -9,38 +8,30 @@ describe('LambdaStack', () => {
     const stack = new Stack();
 
     const lambdaStack = new LambdaStack(stack, 'TestStack', {
-      functionName: 'TestFunction',
-      handler: 'updateMetadata.handler',
-      codePath: 'build/functions',
+      functionName: 'update-metadata-lambda',
+      entry: 'build/functions/updateMetadata/index.js',
+      bucketArn: 'arn:aws:s3:::FAKE_BUCKET_ARN',
     });
 
     const template = Template.fromStack(lambdaStack);
 
     template.hasResource(
       'AWS::Lambda::Function',
-      Match.objectEquals({
-        Runtime: Runtime.NODEJS_14_X.toString(),
-        Handler: 'index.handler',
-        Timeout: Duration.seconds(30).toSeconds(),
+      Match.objectLike({
+        Properties: {
+          FunctionName: 'update-metadata-lambda',
+          Handler: 'index.default',
+          Runtime: 'nodejs14.x',
+        },
       }),
     );
-  });
 
-  test('should output the function ARN', () => {
-    const stack = new Stack();
-
-    const lambdaStack = new LambdaStack(stack, 'TestStack', {
-      functionName: 'TestFunction',
-      handler: 'updateMetadata.handler',
-      codePath: 'build/functions',
-    });
-
-    const template = Template.fromStack(lambdaStack);
-
-    template.hasResource(
-      'AWS::CloudFormation::Output',
-      Match.objectEquals({
-        ExportName: 'TestFunction-output-function-arn',
+    template.hasOutput(
+      '*',
+      Match.objectLike({
+        Value: {
+          'Fn::GetAtt': ['updatemetadatalambdaBF481DB0', 'Arn'],
+        },
       }),
     );
   });
